6c9e454702823009a5b47333620941cabcca566e
RanieroCompagnoni <compagnoni.1985039@studenti.uniroma1.it>
2026-04-02 16:07:51 +0200

    aggiutno tutto i lresto questo terminal client "richiama" le funzioni del server trasforma in json e prende i dati che servono impelmentato get_balance get_inventory make_purchase sfruttano le stesse funzioni ma in server.py

--- e_store_project/src/e_store/terminal_client_ui.py
+++ e_store_project/src/e_store/terminal_client_ui.py
@@ -1,7 +1,7 @@
 import requests
 from typing import Tuple
 
-BASE_URL = "http://127.0.0.1:8007"
+BASE_URL = "http://127.0.0.1:8000"
 
 def get_balance(username:str)->float:
     return requests.get(f"{BASE_URL}/user/{username}/balance").json()["balance"]
@@ -11,11 +11,45 @@ def ask_credentials() ->Tuple[str,str]:
     password = input("Inserisci password: ").strip()
     return username, password
 
+def get_inventory() -> None:
+    print("Store Inventory: ")
+    inventory =  requests.get(f"{BASE_URL}/inventory").json()
+    for item in inventory:
+        print(f"{item["name"]} : {item["quantity"]} at price {item["price"]}")
+
+def make_purchase(username: str, password: str, item_name: str, quantity : int) -> None:
+    payload = {
+        "username": username,
+        "password": password,
+        "item_name": item_name,
+        "quantity": quantity
+    }
+    data = requests.post(f"{BASE_URL}/purchase", json=payload).json()
+    print("Purchase successful!" if data.get("success") else "Purchase failed!")
+    print(data.get("message"))
+
 def run_client():
     print("Welcome to E-store (client/server) user interface")
     username , password = ask_credentials()
 
     while True:
-       balance = get_balance(username)
-        
-    pass
+        balance = get_balance(username)
+        print(f"Your balance:{balance}")
+
+        get_inventory()
+
+        choice = input("Enter item to buy or 'quit' to exit : ").strip()
+        if choice.lower()=="quit":
+            print("Thank you for using the E-Store Client!")
+            break
+            
+        try:
+            quantity = int(input("Enter quantity: "))
+        except ValueError:
+            print("Invalid quantity. Please enter a number.")
+            continue
+    
+        make_purchase(username, password, choice, quantity)
+
+if __name__=="__main__":
+    run_client()
